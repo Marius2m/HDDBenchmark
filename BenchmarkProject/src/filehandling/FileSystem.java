f04676b582d519a09a204b96a5733fa782504c27
George Oprea <georgeoprea10@gmail.com>
2017-05-10 10:08:35 +0300

    pathBuilder implementat

--- BenchmarkProject/src/filehandling/FileSystem.java
+++ BenchmarkProject/src/filehandling/FileSystem.java
@@ -2,8 +2,33 @@ package filehandling;
 
 public class FileSystem {
 
-	public FileSystem() {
-		// TODO Auto-generated constructor stub
-	}
+    private String username;
+    private String os;
+    private String desktopPath;
 
+    public FileSystem() {
+        // TODO Auto-generated constructor stub
+    }
+
+    private void getUsername() {
+        username = System.getProperty("user.name");
+    }
+
+    private void getOS() {
+        os = System.getProperty("os.name");
+    }
+
+    private void pathBuilder() {
+        if (os.equals("Linux")) {
+            desktopPath = "/home/" + username + "/Desktop";
+        } else if (os.contains("Windows")) {
+            desktopPath = "C:\\Users\\" + username + "\\Desktop";
+        } else if (os.toLowerCase().contains("mac")) {
+            desktopPath = "/Users/" + username + "/Desktop";
+        }
+    }
+
+    public String getDesktopPath() {
+        return desktopPath;
+    }
 }
