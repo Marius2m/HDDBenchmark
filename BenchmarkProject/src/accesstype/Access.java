ce2cdd096e1e3b0e42154d03b41ccafa0d922878
George Oprea <georgeoprea10@gmail.com>
2017-05-13 21:36:02 +0300

    Modificari Nadina: ReadSpeed, SequentialAccess

--- BenchmarkProject/src/accesstype/Access.java
+++ BenchmarkProject/src/accesstype/Access.java
@@ -2,13 +2,15 @@ package accesstype;
 
 public abstract class Access {
 
-	public void read(int blockSize, double fileSize){
+    public void read(int fileSize, int blockSize) {
+    }
 
-	}
+    public void write(int fileSize) {
+    }
 
-	public void write(int blockSize, int fileSize){
+    public void write(int fileSize, int blockSize) {
 
-	}
+    }
 
 
 }
--- BenchmarkProject/src/accesstype/SequentialAccess.java
+++ BenchmarkProject/src/accesstype/SequentialAccess.java
@@ -1,37 +1,94 @@
 package accesstype;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Random;
 
 import filehandling.FileSystem;
 
 public class SequentialAccess extends Access {
 
-	private File file;
-	private FileSystem fs;
-	//private Block block;
+    private File file;
+    private FileSystem fs;
+    //private ByteBuffer buf;
+    private static final int BUFFER_FILL_SIZE = 4 * 1024 * 1024; // 4 MB for the buffer filler
+    private static final int GB_TO_BYTE = 1024 * 1024 * 1024;    // for conversion of the file size from GB to bytes
+    private int numTests;    //how many tests will be run on the current benchmark
+    private static int cntTests;
 
-	public SequentialAccess() {
-		fs = new FileSystem();
-		file = new File(fs.getFilePath());
-	}
+    public SequentialAccess(int numTests) {
+        this.numTests = numTests;
+        fs = new FileSystem();               //use a FileSystem object in order to construct the path of the file
+        file = new File(fs.getFilePath());   //open a new temp file with the specified path
+    }
 
-	public void read(int bufferSize, int fileSize) {
-		ByteBuffer buf = ByteBuffer.allocateDirect(bufferSize);
-		FileChannel channel;
+    public void read(int fileSize, int bufferSize) {
+        long fSize = GB_TO_BYTE;     //convert the file size from GB to bytes
+        fSize = fileSize * fSize;
+        ByteBuffer buf = ByteBuffer.allocate(bufferSize);
+        FileChannel inChannel;
+        FileInputStream in;
+        long bytesRead = 0;
 
-		try {
-			channel = (new FileInputStream(file)).getChannel();
-			while(channel.read(buf) != -1)
-				buf.clear();
-			channel.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        try {
+            in = new FileInputStream(file);        //get file input stream for reading from the file
+            inChannel = in.getChannel();           //get the associated channel of the file
+            while (inChannel.read(buf) != -1) {      //while there still are bytes to be read from file, continue
+                buf.clear();                       //clear the buffer so the next input can be stored correctly
+                bytesRead += bufferSize;
+            }
+            inChannel.close();
+            in.close();
+            if (shouldDeleteFile())
+                fs.deleteFile(file);               //delete the temp file after benchmark finishes
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	}
+    private boolean shouldDeleteFile() {
+        cntTests++;
+        if (numTests == cntTests) {     //when all tests were performed send message to delete file
+            cntTests = 0;
+            return true;
+        } else
+            return false;
+    }
+
+
+    public void write(int fileSize) {         //overloaded method for writing the temp file used for reading
+        write(fileSize, BUFFER_FILL_SIZE);    //will write the file with a specified buffer size and file size
+    }
+
+    public void write(int fileSize, int bufferSize) {
+        BufferedOutputStream out;
+        long crntWrittenSize = 0;
+        long fSize = GB_TO_BYTE;      //convert the file size from GB to bytes
+        fSize = fileSize * fSize;
+        byte[] buf = new byte[bufferSize];
+
+        try {
+            out = new BufferedOutputStream(new FileOutputStream(file));    //get buffered file output stream for writing in the file
+            while (crntWrittenSize < fSize) {          //continue writing until the file size was reached
+                fillBuffer(buf);                    //add random bytes into a buffer of the given size
+                out.write(buf);                    //write these bytes through the channel into the file
+                crntWrittenSize += bufferSize;      //update the total amount of bytes written so far in the file
+            }
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fillBuffer(byte[] bytes) {
+        Random rand = new Random();
+        rand.nextBytes(bytes);
+    }
+
 }
