a91e39208ee27c24c6e866289e479806e865db1d
George Oprea <georgeoprea10@gmail.com>
2017-05-13 21:12:39 +0300

    ADDED: min, max, average.

--- BenchmarkProject/src/score/Score.java
+++ BenchmarkProject/src/score/Score.java
@@ -8,12 +8,15 @@ public class Score {
     private double fileSize;       //size of File
     private Timer timer = new Timer();
     private int counter = 0;
+    private int nTests;
     private double avgSpeed = 0;
+    private double minSpeed = Double.MAX_VALUE;
+    private double maxSpeed = 0;
 
     public Score(int n, double fileSize) {
         // TODO Auto-generated constructor stub
-        this.fileSize = fileSize;
-        times = new double[n];
+        this.fileSize = fileSize * 1024;        //convert GB to MB
+        this.nTests = n;
     }
 
     public void start() {
@@ -21,18 +24,26 @@ public class Score {
         counter++;
     }
 
-    public void stop() {
+    public double stop() {
         long time = timer.stop();
-        times[counter] = time;
+        double score;
+        score = fileSize / (time / (1000 * 1000 * 1000));  // MB/s
+        avgSpeed += score;
+
+        if (score < minSpeed) minSpeed = score;
+        else if (score > maxSpeed) maxSpeed = score;
+
+        if (counter == nTests) avgSpeed /= nTests;
+        return score;
     }
 
-    private void avgSpeed(){
-        double sum=0;
-        for(int i = 0; i<counter; i++){
-            sum += times[i];
+    private void avgSpeed() {
+        double sum = 0;
+        for (int i = 0; i < counter; i++) {
         }
-        avgSpeed = sum/counter;
+        avgSpeed = sum / counter;
     }
+
 
     public double getScore() {
         this.stop();
@@ -41,8 +52,20 @@ public class Score {
         return score;
     }
 
+    public double getMinSpeed() {
+        return minSpeed;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public double getAvgSpeed() {
+        return avgSpeed;
+    }
+    
     public String toString() {
-        String message = (1024 * fileSize) / (times[0] / 1000 * 1000 * 1000) + " MB/s";
+        String message = fileSize / (times[0] / 1000 * 1000 * 1000) + " MB/s";
         return message;
     }
 
