a3de3ff2cfd3587bbd65ca46be76fb80f2723c58
George Oprea <georgeoprea10@gmail.com>
2017-05-13 19:06:54 +0300

    update score

--- BenchmarkProject/src/score/Score.java
+++ BenchmarkProject/src/score/Score.java
@@ -5,37 +5,44 @@ import java.util.ArrayList;
 public class Score {
 
     private double[] times;
-    private int nTests;         //number of tests
     private double fileSize;       //size of File
     private Timer timer = new Timer();
-    private static int counter = 0;
+    private int counter = 0;
+    private double avgSpeed = 0;
 
     public Score(int n, double fileSize) {
         // TODO Auto-generated constructor stub
         this.fileSize = fileSize;
-        this.nTests = n;
         times = new double[n];
     }
 
     public void start() {
         timer.start();
+        counter++;
     }
 
     public void stop() {
         long time = timer.stop();
         times[counter] = time;
-        counter++;
+    }
+
+    private void avgSpeed(){
+        double sum=0;
+        for(int i = 0; i<counter; i++){
+            sum += times[i];
+        }
+        avgSpeed = sum/counter;
     }
 
     public double getScore() {
         this.stop();
         double score;
-        score = (1024 * fileSize) / times[0];  // MB/s
+        score = (1024 * fileSize) / (times[0] / 1000 * 1000 * 1000);  // MB/s
         return score;
     }
 
     public String toString() {
-        String message = (1024 * fileSize) / times[0] + " MB/s";
+        String message = (1024 * fileSize) / (times[0] / 1000 * 1000 * 1000) + " MB/s";
         return message;
     }
 
