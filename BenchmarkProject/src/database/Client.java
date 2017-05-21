2ced36dcdb4c4a52e5430c4a657f8561597ac618
leooonard8 <leonard.filip.93@gmail.com>
2017-05-21 10:47:45 +0300

    Update Client.java

--- BenchmarkProject/src/database/Client.java
+++ BenchmarkProject/src/database/Client.java
@@ -4,7 +4,9 @@ public class Client {
 
 	public static void main(String[] args) throws SQLException {
 		new BenchMarkDatabase();
+		BenchMarkDatabase.insertRow("Popescu", "HDD", "Asus Brutus", 88.9);
 		BenchMarkDatabase.displayDatabase();
+		//updates:dynamic table search + column header click-sort
 		
 
 	}
